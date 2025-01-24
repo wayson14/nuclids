53ded10bda7ef623203041c57523976abe5c9b10
wayson14 <wayson.1104@gmail.com>
2025-01-24 12:47:59 +0100

    MVP - scraping all evaporations

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 import json
 import csv
+import periodictable as pt
 
 # Making a GET request
 # r = requests.get('http://nrv.jinr.ru/nrv/webnrv/expdata/get_reaction.php?task=evaporation_residues&id=285&p=&n=&a=&channel=')
@@ -20,6 +21,7 @@ def get_reaction_data(id=1):
     # soup = bs(response.content, 'html.parser')
     response.encoding = "utf-8-sig"
     valid_data = response.json()
+    print(f"relcode: {id}")
     print(valid_data["data"])
     return valid_data
 
@@ -44,6 +46,12 @@ class Reaction:
             else:
                 self.data = []
                 self.parse_experimental_data(value)
+
+    def get_element_symbol_by_z(self, z) -> str:
+        return pt.elements[z].symbol
+
+    def get_shortened_reaction_label(self) -> str:
+        return f"({self.a_proj}){self.elem_proj}+({self.a_targ}){self.elem_targ} -> ({int(self.a_proj)+int(self.a_targ)}){self.get_element_symbol_by_z(int(self.z_proj)+int(self.z_targ))}"
 
     def parse_experimental_data(self, value):
         rows = value.split("\n")
@@ -82,15 +90,14 @@ class ReactionTable(list):
     def combine_experimental_data_rows_with_ids(self) -> list:
         buffer = []
         for item in self[:]:
-            item_relcode = item.relcode
+            # item_relcode = item.relcode
             for data_row_dict in item.data:
-                row = [item_relcode]
+                row = [item.relcode, item.get_shortened_reaction_label()]
                 row += data_row_dict.values()
                 buffer.append(row)
         return buffer
 
-    def save_results_table_to_csv(self):
-        filename = "results.csv"
+    def save_results_table_to_csv(self, filename: str = "results.csv"):
         items = self.combine_experimental_data_rows_with_ids()
         # keys = self[0].__dict__
         try:
@@ -99,6 +106,7 @@ class ReactionTable(list):
                 writer.writerow(
                     [
                         "relcode",
+                        "shortened_label",
                         "energy",
                         "cross_section",
                         "error_plus",
@@ -115,15 +123,31 @@ class ReactionTable(list):
         pass
 
 
+def scrape_all_evaporation_data_to_csv(min_relcode=1, max_relcode=300):
+    reaction_table = ReactionTable()
+    for relcode in range(min_relcode, max_relcode + 1):
+        try:
+            reaction_data = get_reaction_data(relcode)
+            reaction = Reaction(reaction_data)
+            reaction_table.append(reaction)
+        except BaseException:
+            continue
+    reaction_table.save_results_table_to_csv()
+
+
 if __name__ == "__main__":
     # get_reaction_data()
-    # EASY TESTING
-    sample_reaction_data = get_reaction_data(id=1)
-    r1 = Reaction(sample_reaction_data)
-    r2 = Reaction(sample_reaction_data)
-    r_table = ReactionTable()
-    r_table.append(r1)
-    r_table.append(r2)
-    to_csv = r_table.combine_experimental_data_rows_with_ids()
-    print(to_csv)
-    r_table.save_results_table_to_csv()
+
+    # EASY TESTING #1
+    # sample_reaction_data = get_reaction_data(id=1)
+    # r1 = Reaction(sample_reaction_data)
+    # r2 = Reaction(sample_reaction_data)
+    # r_table = ReactionTable()
+    # r_table.append(r1)
+    # r_table.append(r2)
+    # to_csv = r_table.combine_experimental_data_rows_with_ids()
+    # print(to_csv)
+    # r_table.save_results_table_to_csv()
+
+    # INTEGRATION TESTING
+    scrape_all_evaporation_data_to_csv(1, 305)
