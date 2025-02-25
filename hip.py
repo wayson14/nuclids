07989900e84f4de4ab7e0b81c2ffe1912acd904f
wayson14 <wayson.1104@gmail.com>
2025-02-25 18:42:17 +0100

    Working batch preparing of hivapein files from the csv of a given format

--- hip.py
+++ hip.py
@@ -14,7 +14,7 @@ class Experiment:
 
     def __init__(self, data_dict):
 
-        self.relccode = data_dict["relcode"]
+        self.relcode = data_dict["relcode"]
         self.shortened_label: int = data_dict["shortened_label"]
         self.a_proj: int = data_dict["a_proj"]
         self.z_proj: int = data_dict["z_proj"]
@@ -27,6 +27,8 @@ class Experiment:
         self.channel_n = channel_vector[0]
         self.channel_p = channel_vector[1]
         self.channel_a = channel_vector[2]
+
+        self.input_file_string = ""
         pass
 
     def parse_shortened_label(self) -> tuple[int, int, int, int]:
@@ -41,15 +43,41 @@ class Experiment:
     def append_energy_series(self, energy_value: int):
         self.energy_data_series.append(energy_value)
 
-    def prepare_hivap_input_file(self):
+    def generate_hivapein(self):
+        self.prepare_hivap_input_file_content()
+        self.write_hivap_input_file()
+
+    def prepare_hivap_input_file_content(self):
         energy_value = 0
-        energy_series_template_string = f" E={energy_value} IEXC 0 IFUS=11 LIMBAR= 1 JLOWER 0 JUPPER 0 0 0 0 ENERGY1= 0\nV0=59  r0=1.1477 D=0.644 Q2=0   CRED=1.0  NOCURV=0 NOPROX=0  IOPT=0\nITEST=0 sigr=2.2 cutoff=2.90 xth=0.720 APUSH=15.0  FPUSH=0.75  SPUSH=1\n-----------------------------------------------------------------------\n"
+        energy_series_template_string = " E={energy_value} IEXC 0 IFUS=11 LIMBAR= 1 JLOWER 0 JUPPER 0 0 0 0 ENERGY1= 0\nV0=59  r0=1.1477 D=0.644 Q2=0   CRED=1.0  NOCURV=0 NOPROX=0  IOPT=0\nITEST=0 sigr=2.2 cutoff=2.90 xth=0.720 APUSH=15.0  FPUSH=0.75  SPUSH=1\n-----------------------------------------------------------------------\n"
+        energy_input_str = ""
+
+        for energy_value in self.energy_data_series:
+            energy_input_str += energy_series_template_string.format(
+                energy_value=energy_value
+            )
+
         with open("hivapein_template.dat", "r") as template:
             content = template.read()
-            content.replace("{shortened_label}", self.shortened_label)
-            for energy_value in self.energy_data_series:
-                content.replace("{energies_marker}", energy_series_template_string)
-                # TODO: there I finished
+            content = content.format(
+                shortened_label=self.shortened_label,
+                a_proj=self.a_proj,
+                z_proj=self.z_proj,
+                a_targ=self.a_targ,
+                z_targ=self.z_targ,
+                channel_n=self.channel_n,
+                channel_a=self.channel_a,
+                channel_p=self.channel_p,
+                energies_marker=energy_input_str,
+            )
+
+            self.input_file_string = content
+        return
+
+    def write_hivap_input_file(self):
+        with open(f"hivapein.dat.{self.relcode}", "w") as f:
+            f.write(self.input_file_string)
+        return
 
 
 def load_rows(filename: str) -> list[dict]:
@@ -88,13 +116,13 @@ def experiment_generator(csv_filename="results.csv"):
     Hivap input files (hivapein[n].dat) later used to perform simulations
     (method from Experiment class, so every Experiment creates its own file)"""
 
-    data = load_rows(csv_filename)
+    csv_dicts = load_rows(csv_filename)
     experiment_dict = {}
     relcodes_table = []
     # def check_for_relcode(relcode, exp):
 
     # going through rows
-    for row in data:
+    for row in csv_dicts:
         if row["relcode"] not in relcodes_table:
             experiment = Experiment(row)
             experiment_dict[row["relcode"]] = experiment
@@ -103,6 +131,15 @@ def experiment_generator(csv_filename="results.csv"):
             experiment: Experiment = experiment_dict[row["relcode"]]
             experiment.append_energy_series(row["energy"])
 
+    for key in experiment_dict.keys():
+        if int(key) < 20:
+            experiment_obj = experiment_dict[key]
+            experiment_obj.generate_hivapein()
+        else:
+            break
+    return
+    # sample_experiment: Experiment = experiment_dict["1"]
+    # sample_experiment.generate_hivapein()
     # now experiment_dict should be full of experiment data
 
 
